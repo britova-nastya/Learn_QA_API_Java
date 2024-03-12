3ee06c381aad430b6652065d6ccbe2415021245f
Anastasia Britova <ABritova@alfabank.ru>
2024-03-12 11:03:24 +0300

    Sending a Get request

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWord(){
-        System.out.println("Hello Word");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
